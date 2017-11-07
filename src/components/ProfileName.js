9621b67a97b36984cd431e460572db4d18b3a171
Richard Hill III <rhillz718@gmail.com>
2017-11-07 00:04:34 -0500

    updated sign in with ability to sign out and refactered code

---
+++ src/components/ProfileName.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+import {Text, View, TextInput} from 'react-native';
+
+
+const ProfileName = () =>{
+
+    return (
+        <View>
+            <Text>
+                Please Enter Your Name
+            </Text>
+            <TextInput/>
+            
+        
+        </View>
+    )
+}
+
+export default ProfileName
